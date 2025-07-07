ff0515e92ed0426376ce8235ee80d5b54d631493
unknown <harha@gmail.com>
2025-07-07 18:35:53 +0530

    terraform

--- 40-database/bootstrap.sh
+++ 40-database/bootstrap.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-set -e
-set -x  # enables command tracing
 
 component=$1
-env=$2
-
-dnf install -y git ansible
-ansible-pull -U https://github.com/harshavardhan-nandigama/ansible-roles-tf.git -e component=$component -e env=$env main.yaml
+dnf install ansible -y
+ansible-pull -U https://github.com/harshavardhan-nandigama/ansible-roles-tf.git -e component=$1 -e env=$2 main.yaml
