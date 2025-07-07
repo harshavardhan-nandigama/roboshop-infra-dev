b063a9dba262fdd7bdd19243f1fd3c837f3ecd94
unknown <harha@gmail.com>
2025-07-07 14:20:13 +0530

    terraform

---
+++ 30-vpn/variables.tf
@@ -0,0 +1,15 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_id" {
+    default = "Z07069691890X06YTPXD4"
+}
+
+variable "zone_name" {
+    default = "harshavn24.site"
+}
